9f1c2c1d773307b16197afde24a248ba039e7617
Adir Barak <adirbarak1@gmail.com>
2023-11-29 17:16:24 +0200

    initing project

---
+++ cmake-build-debug/CMakeFiles/pic2ascii.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pic2ascii.dir/main.cpp.o"
+  "pic2ascii"
+  "pic2ascii.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pic2ascii.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
